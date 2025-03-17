ef669ff08c061bf8212626eb54da0a6c4624ec20
Dahyunlee <dahoneyee@gmail.com>
2025-03-17 11:58:20 +0900

    2주차 실습

---
+++ inputTest.py
@@ -0,0 +1,8 @@
+num1 = input("숫자1 ==>")
+num1 = int(num1)
+
+num2 = input("숫자2 ==>")
+num2 = int(num2)
+
+numSum = num1 + num2
+print(numSum)
---
+++ 변수.py
@@ -0,0 +1,15 @@
+a = 100
+b = 200
+print(a)
+print(b)
+c = a + b
+print(a "+",b "=",c)
+d = a - b
+print(a, "-",b,'=',d)
+e = a + b
+print(a, "-",b,"=",e)
+f = a / b
+print(a,"+",b, "=",f)
+
+
+
