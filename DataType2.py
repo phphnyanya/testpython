e79a9875a76fe338010a5ecd9e55f86b3595d443
Dahyun lee <dahoneyee@gmail.com>
2025-04-07 10:53:39 +0900

    4주차 과제

---
+++ DataType2.py
@@ -0,0 +1,36 @@
+import tkinter
+import random
+
+root = tkinter.Tk()
+root.title("캔버스 만들기")
+
+# 좌표 출력기
+def mouseMove(event):
+    x = event.x
+    y = event.y
+    labelMouse["text"] = str(x) + "," + str(y)
+
+def click_btn():
+    label["text"] = random.choice(["대길","중길","소길","흉"])
+
+# 캔버스 생성
+canvas = tkinter.Canvas(root, width=800, height=600, bg="skyblue")
+canvas.pack()
+
+# 좌표 출력기
+labelMouse = tkinter.Label(root, text=",", font=("맑은 고딕", 10)) 
+labelMouse.pack()
+
+# 이미지 불러오기
+bgimg = tkinter.PhotoImage(file="miko.png") 
+canvas.create_image(400, 300, image=bgimg)
+
+# 텍스트 라벨
+label = tkinter.Label(root, text="??", font=("맑은 고딕", 120))  
+label.place(x=380, y=60)
+
+# 제비뽑기 버튼
+button = tkinter.Button(root, text="제비뽑기", font=("맑은 고딕", 36),command=click_btn)
+button.place(x=360, y=400)
+
+root.mainloop()
